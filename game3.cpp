b09173a7f84f46e4fc0fbe4fed573d531e16ad89
pattgene <101797025+pattgene@users.noreply.github.com>
2023-04-19 04:09:31 +0800

    debug return value, using dynamic memory instead.

--- game3.cpp
+++ game3.cpp
@@ -4,13 +4,24 @@
 
 using namespace std;
 
-int game3() {
+int game3(string playername) {
+    cout << "Welcome, " << playername << ", to Game 3." <<endl;
     cout << "Welcome to the Fruit Slot Machine!" << endl;
     cout << "If you get 3 of the same fruit, you will win 4 points back" << endl;
     cout << "Otherwise, you will lose everything" << endl;
     cout << "Press ENTER to spin the reels! ";
     srand(time(0));
-    string emojis[10] = {"🍎", "🍊", "🍇", "🍒", "🍓", "🍋", "🍉", "🍌", "🍑", "🍐"};
+    string* emojis = new string[10];
+    emojis[0] = "🍎";
+    emojis[1] = "🍊";
+    emojis[2] = "🍇";
+    emojis[3] = "🍒";
+    emojis[4] = "🍓";
+    emojis[5] = "🍋";
+    emojis[6] = "🍉";
+    emojis[7] = "🍌";
+    emojis[8] = "🍑";
+    emojis[9] = "🍐";
     string slot1 = emojis[rand() % 10];
     string slot2 = emojis[rand() % 10];
     string slot3 = emojis[rand() % 10];
@@ -25,7 +36,7 @@ int game3() {
             cout << "┏━━━┳━━━┳━━━┓" << endl;
             cout << "┃ " << emojis[rand() % 10] << "┃ " << emojis[rand() % 10] << "┃ " << emojis[rand() % 10] << "┃" << endl;
             cout << "┗━━━┻━━━┻━━━┛" << endl;
-            cout << "Press ENTER to stop the reels!";
+            cout << "Keep Pressing ENTER to stop the reels!";
             cin.get();
         }
         cout << "\033[2J\033[1;1H";
@@ -35,11 +46,15 @@ int game3() {
         if (slot1 == slot2 && slot2 == slot3) {
             cout << "Congratulations, you won 4 points!" << endl;
             cout << "Thanks for playing the Fruit Slot Machine!" << endl;
+            delete[] emojis;
             return 4;
         } else {
             cout << "Sorry, you lost everything." << endl;
             cout << "Thanks for playing the Fruit Slot Machine!" << endl;
+            delete[] emojis;
             return 0;
         }
     }
+    delete[] emojis;
+    return 0;
 }
