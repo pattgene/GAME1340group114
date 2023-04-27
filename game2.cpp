c16e538d27e80552ccaba583af857c0cc043bd61
warrenleander <129947759+warrenleander@users.noreply.github.com>
2023-04-27 16:30:29 +0800

    Update game2.cpp
    Added in feature which allows player to split their hands and play them separately if they got a hand with a pair.

--- game2.cpp
+++ game2.cpp
@@ -53,6 +53,11 @@ void display_hand(vector<string>& hand, int& score) {
     cout << endl << "Your score: " << score << endl;
 }
 
+// Returns True or False based on whether the hand can be split or not
+bool can_split(vector<string>& hand) {
+    return hand.size() == 2 && hand[0] == hand[1];
+}
+
 int game2(std::string playername) {
     cout << "Welcome, " << playername << ", to Game 2." <<endl;
     cout << "THIS GAME IS BLACK JACK!!!!! " <<endl;
@@ -70,8 +75,85 @@ int game2(std::string playername) {
     deal_card(deck_copy, dealer_hand, dealer_score);
     deal_card(deck_copy, player_hand, player_score);
     deal_card(deck_copy, dealer_hand, dealer_score);
+    
+    bool has_split = false;
+    vector<string> player_hand2;
+    int player_score2 = 0;
 
     display_hand(player_hand, player_score);
+    
+    // Asks player if they would like to split their hand
+    if (can_split(player_hand)) {
+        char split_choice;
+        cout << "You have a pair. Would you like to split? (Y/N): ";
+        cin >> split_choice;
+        while (!(split_choice == 'Y' || split_choice == 'y' || split_choice == 'N' || split_choice == 'n')) {
+            cout << "Invalid input, please try again.";
+            cout << "Split? (Y/N): ";
+            cin >> split_choice;
+        }
+        // Splitting program
+        if (split_choice == 'Y' || split_choice == 'y') {
+            has_split = true;
+            player_hand2.push_back(player_hand[1]);
+            player_hand.pop_back();
+            player_score2 = player_score / 2;
+            player_score = player_score2;
+            deal_card(deck_copy, player_hand, player_score);
+            deal_card(deck_copy, player_hand2, player_score2);
+            display_hand("Your hand: ", player_hand, player_score);
+            display_hand("Your split hand: ", player_hand2, player_score2);
+        }
+  
+
+        // Play each hand, including the split hand if applicable.
+        vector<vector<string>> hands = {player_hand};
+        vector<int> scores = {player_score};
+        if (has_split) {
+            hands.push_back(player_hand2);
+            scores.push_back(player_score2);
+        }
+
+        for (int hand_idx = 0; hand_idx < int(hands.size()); ++hand_idx) {
+            string hand_label = has_split ? (hand_idx == 0 ? "Your hand" : "Your split hand") : "Your hand";
+            while (scores[hand_idx] < 21) {
+                char choice;
+                cout << hand_label << "Hit or stand? (H/S): ";
+                cin >> choice;
+                while (!(choice == 'H' || choice == 'h' || choice == 'S' || choice == 's')) {
+                    cout << "Invalid input, please try again.";
+                    cout << hand_label << "Hit or stand? (H/S): ";
+                    cin >> choice;
+                }
+                if (choice == 'H' || choice == 'h') {
+                    deal_card(deck_copy, hands[hand_idx], scores[hand_idx]);
+                    display_hand(hand_label, hands[hand_idx], scores[hand_idx]);
+                } else {
+                    break;
+                }
+            }
+        }
+        // Calculating winner for split hand deck
+        int points = 0;
+        for (int hand_idx = 0; hand_idx < int(hands.size()); ++hand_idx) {
+            string hand_label = has_split ? (hand_idx == 0 ? "Your hand" : "Your split hand") : "Your hand";
+            if (scores[hand_idx] > 21) {
+                cout << hand_label << " - Bust! You lose." << endl;
+            } else if (dealer_score > 21) {
+                cout << hand_label << " - Dealer busts! You win." << endl;
+                points += 4;
+            } else if (scores[hand_idx] > dealer_score) {
+                cout << hand_label << " - You win!" << endl;
+                points += 4;
+            } else if (dealer_score > scores[hand_idx]) {
+                cout << hand_label << " - You lose." << endl;
+            } else {
+                cout << hand_label << " - Draw" << endl;
+            }
+        }
+        return points;
+
+    }
 
     while (player_score < 21) {
         char choice;
